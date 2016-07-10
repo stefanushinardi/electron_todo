7346ddb56fff6cccb02635a28d7ae4ae50227947
Xun <xunchen369@gmail.com>
2016-07-10 00:18:25 -0700

    added redux implementation

---
+++ generator/presentation/temp.presentation.js
@@ -0,0 +1,12 @@
+const <%= name %>Presentation = (props) => {
+  //<div>{props.children}</div>;
+  <div id="<%= name %>Presentation"><%= name %>Presentation</div>;
+}
+<%= name %>Presentation.propTypes = {
+  //children: React.PropTypes.element.isRequired
+};
+<%= name %>Presentation.defaultProps = {
+  //children: React.PropTypes.element.isRequired
+};
+
+export default <%= name %>Presentation;
