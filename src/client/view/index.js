c26b40a0ae08e0525684820f368db6ff3c7317fb
Xun <xunchen369@gmail.com>
2016-07-10 00:18:20 -0700

    added redux implementation

--- src/client/view/index.js
+++ src/client/view/index.js
@@ -1,11 +1,16 @@
 import React, { Component, PropTypes } from 'react';
 import classNames from 'classnames';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux'
+import * as TodoActions from '../actions';
 
 class Index extends Component{
   constructor(props){
     super(props);
   }
   render() {
+    const { todos, actions } = this.props;
+    console.log(todos,actions);
     return (
       <div id="index" className="row">
         <div className="col-xs-12">
@@ -16,4 +21,24 @@ class Index extends Component{
   }
 }
 
-export default Index;
+Index.propTypes = {
+  todos: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired
+}
+
+function mapStateToProps(state){
+  return {
+    todos: state.todos
+  }
+}
+
+function mapDispatchToProps(dispatch){
+  return {
+    actions: bindActionCreators(TodoActions, dispatch)
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Index);
